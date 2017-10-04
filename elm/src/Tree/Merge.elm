e45b224a3576f2d1f80bb42e31bebfcf0f68793c
Matthew Buscemi <matthew.buscemi@gmail.com>
2017-10-03 20:11:41 -0700

    the Tree code is so nearly right... so nearly...

---
+++ elm/src/Tree/Merge.elm
@@ -0,0 +1,51 @@
+module Tree.Merge exposing (fromPath)
+
+import Tree.Tree exposing (Tree(Node))
+
+
+fromPath : List String -> Tree String -> Tree String
+fromPath list tree =
+    case ( list, tree ) of
+        ( listField :: rest, Node treeField [] ) ->
+            if listField == treeField then
+                tree
+            else
+                Node treeField [ fromPath rest (Node listField []) ]
+
+        ( listField :: nextField :: rest, Node treeField subNodes ) ->
+            if listField == treeField then
+                Node treeField (List.append (excludeSubNode nextField subNodes) [ fromPath rest (Node nextField []) ])
+            else
+                Node treeField (List.append subNodes [ fromPath (nextField :: rest) (Node listField []) ])
+
+        ( listField :: rest, Node treeField subNodes ) ->
+            Node treeField (List.append subNodes [ fromPath rest (Node listField []) ])
+
+        ( [], Node treeField [] ) ->
+            Node treeField []
+
+        ( [], Node treeField subNodes ) ->
+            Node treeField subNodes
+
+
+excludeSubNode : String -> List (Tree String) -> List (Tree String)
+excludeSubNode field subNodes =
+    List.filter
+        (\(Node nodeField _) -> nodeField /= field)
+        subNodes
+
+
+merge : String -> Tree String -> Tree String -> Tree String
+merge field (Node _ subNodesA) (Node _ subNodesB) =
+    Node field (List.append subNodesA subNodesB)
+
+
+containsNodeWith : String -> List (Tree String) -> Bool
+containsNodeWith field treeList =
+    List.filter
+        (\(Node nodeField _) ->
+            field == nodeField
+        )
+        treeList
+        |> List.length
+        |> (>=) 1
--- elm/tests/Tests.elm
+++ elm/tests/Tests.elm
@@ -1,29 +1,86 @@
-module Tests exposing (..)
+module Tests exposing (suite)
 
-import Expect exposing (Expectation)
-import Test exposing (..)
+import Expect
+import Test exposing (Test, describe, test)
+import Tree.Merge exposing (fromPath)
+import Tree.Tree exposing (Tree(Node))
 
 
 suite : Test
 suite =
-    describe "Elm Test Runner" <|
-        [ describe "boilerplate" <|
-            [ test "something dumb" <|
-                \_ ->
-                    Expect.equal (2 + 2) 4
-            , test "something else" <|
-                \_ ->
-                    Expect.equal ("a" ++ "b") "ab"
-            ]
-        , describe "blarg and frangle" <|
-            [ test "blarg" <|
-                \_ ->
-                    Expect.atMost 5 3
-            , test "frangle" <|
-                \_ ->
-                    Expect.atLeast 3 5
-            , test "blarg & frangle" <|
-                \_ ->
-                    Expect.true "your mom?" True
-            ]
+    describe "Tree.Merge" <|
+        [ test "empty list yields single node" <|
+            \_ ->
+                let
+                    emptyTree =
+                        Node "" []
+                in
+                Expect.equal (fromPath [] emptyTree) emptyTree
+        , test "single element in list yields single child node" <|
+            \_ ->
+                let
+                    emptyTree =
+                        Node "" []
+
+                    treeWithChild =
+                        Node "child" []
+                in
+                Expect.equal (fromPath [ "child" ] emptyTree) (Node "" [ Node "child" [] ])
+        , test "list with depth of three generates tree with depth of three" <|
+            \_ ->
+                let
+                    emptyTree =
+                        Node "parent" []
+
+                    childrenTree =
+                        Node "child1" [ Node "child2" [ Node "child3" [] ] ]
+
+                    treeWithThreeChildren =
+                        Node "parent" [ childrenTree ]
+                in
+                Expect.equal (fromPath [ "child1", "child2", "child3" ] emptyTree) treeWithThreeChildren
+        , test "a new field do not duplicate an existing field" <|
+            \_ ->
+                let
+                    aTree =
+                        Node "a" []
+                in
+                Expect.equal (fromPath [ "a" ] aTree) aTree
+        , test "new fields do not duplicate existing fields" <|
+            \_ ->
+                let
+                    abcTree =
+                        Node "b" [ Node "c" [ Node "d" [] ] ]
+                in
+                Expect.equal (fromPath [ "b", "c", "d" ] abcTree) abcTree
+        , test "new fields are additive" <|
+            \_ ->
+                let
+                    startTree =
+                        Node "e" [ Node "f" [] ]
+
+                    expectedTree =
+                        Node "e" [ Node "f" [], Node "g" [] ]
+                in
+                Expect.equal (fromPath [ "e", "g" ] startTree) expectedTree
+        , test "multiple additive condition" <|
+            \_ ->
+                let
+                    startTree =
+                        Node "h" [ Node "i" [], Node "j" [], Node "k" [] ]
+
+                    expectedTree =
+                        Node "h" [ Node "i" [], Node "j" [], Node "k" [], Node "l" [] ]
+                in
+                Expect.equal (fromPath [ "h", "l" ] startTree) expectedTree
+        , test "complex condition" <|
+            \_ ->
+                let
+                    startTree =
+                        Node "h" [ Node "i" [ Node "l" [], Node "m" [] ], Node "j" [], Node "k" [ Node "n" [], Node "o" [] ] ]
+
+                    expectedTree =
+                        Node "h" [ Node "i" [ Node "l" [], Node "m" [ Node "p" [] ] ], Node "j" [], Node "k" [ Node "n" [], Node "o" [] ] ]
+                in
+                Expect.equal (fromPath [ "h", "i", "m", "p" ] startTree) expectedTree
         ]
