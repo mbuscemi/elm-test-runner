d2d9710c728ad9595bc4b13e1bbaaeb4a453e7be
Matthew Buscemi <matthew.buscemi@gmail.com>
2017-10-15 09:53:50 -0700

    added ExpectOnFail test

---
+++ elm/tests/Decoding/ExpectOnFail.elm
@@ -0,0 +1,63 @@
+module Decoding.ExpectOnFail exposing (parsesCorrectly)
+
+import Expect
+import Maybe.Extra as Maybe
+import State.Failure as Failure exposing (Failure)
+import Test exposing (Test, describe, test)
+import TestEvent.TestCompleted exposing (TestCompleted, parse, toTestInstance)
+import TestInstance.Core as TestInstance exposing (TestInstance)
+
+
+parsesCorrectly : Test
+parsesCorrectly =
+    let
+        event : TestCompleted
+        event =
+            parse failingJsonString
+
+        testInstance : TestInstance
+        testInstance =
+            toTestInstance event
+
+        maybeFailure : Maybe Failure
+        maybeFailure =
+            TestInstance.getFailure testInstance
+
+        failure : Failure
+        failure =
+            Maybe.withDefault Failure.nullInstance maybeFailure
+    in
+    describe "parsesCorrectly" <|
+        [ test "has correct status" <|
+            \_ ->
+                Expect.true "expected failing test" (TestInstance.isFailing testInstance)
+        , test "has a failure" <|
+            \_ ->
+                Expect.true "expected a failure in the failures list" (Maybe.isJust maybeFailure)
+        , test "has correct message" <|
+            \_ ->
+                Expect.equal (Failure.getMessage failure) "thought those two strings would be the same"
+        , test "has no given" <|
+            \_ ->
+                Expect.true "expected not to find a given" (Maybe.isNothing <| Failure.getGiven failure)
+        , test "has a complex comparison" <|
+            \_ ->
+                Expect.false "expected not to find a complex comparison object" (Failure.hasComplexComparison failure)
+        , test "has no expected text" <|
+            \_ ->
+                Expect.equal (Failure.getExpected failure) ""
+        , test "has no actual text" <|
+            \_ ->
+                Expect.equal (Failure.getActual failure) ""
+        , test "should not display with diff" <|
+            \_ ->
+                Expect.false "expected that diff should not occur" (Failure.shouldDiff failure)
+        , test "not a todo" <|
+            \_ ->
+                Expect.false "expected that this is not a todo test" (Failure.isTodo failure)
+        ]
+
+
+failingJsonString : String
+failingJsonString =
+    """{"event":"testCompleted","status":"fail","labels":["FailureOutputValidation","Expect.onFail","documentation example","custom failure message"],"failures":[{"given":null,"message":"thought those two strings would be the same","reason":{"type":"custom","data":"thought those two strings would be the same"}}],"duration":"1"}"""
