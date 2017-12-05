fc0653c52f2a9d7bd14e50a194c8f170145ecb8b
Matthew Buscemi <matthew.buscemi@gmail.com>
2017-12-04 17:21:48 -0800

    deleting unused files

--- spec/elm-test-runner-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ElmTestRunnerView from '../lib/elm-test-runner-view';
-
-describe('ElmTestRunnerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
